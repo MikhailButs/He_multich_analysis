0b51cb55c3b1464c748c8b0ea6386ecec5054b1f
joana <joana.fedorenko@gmail.com>
2022-06-24 23:33:38 +0300

    bug with time-ax fixed and initial time added

--- app.py
+++ app.py
@@ -2,15 +2,22 @@ import os
 import matplotlib.pyplot as plt
 from PyQt5 import QtWidgets
 from get_data import get_data
+from numpy import arange
 
 dir2work = os.path.normpath(input('Input an abs link:    '))
+try:
+    images_list = os.listdir(dir2work)  # ['name1.jpg', 'name2.jpg', ...]
+except FileNotFoundError:#
+    print('Invalid value')
+    exit()
+
 ax_type = input('Frames or Time(ms) X-axis? (F/T):    ')
 time_step = 0
 
-if ax_type == 'F' or 'f':
+if ax_type == 'F' or ax_type == 'f':
     ax_type = 'f'
     print('Frames selected')
-elif ax_type == 'T' or 't':
+elif ax_type == 'T' or ax_type == 't':
     print('Time selected')
     ax_type = 't'
     time_step = input('Set time interval (ms):    ')
@@ -19,16 +26,21 @@ elif ax_type == 'T' or 't':
     except ValueError:
         print('Invalid value')
         exit()
+    initial_time = input('Set initial time (ms):    ')
+    try:
+        initial_time = float(initial_time)
+    except ValueError:
+        print('Invalid value')
+        exit()
 else:
     print('Invalid value')
     exit()
 
-images_list = os.listdir(dir2work)  # ['name1.jpg', 'name2.jpg', ...]
 intensity_list = ['' for i in range(len(images_list))]  # [[ch1, ch2, ch3], [ch1, ch2, ch3], ...]
 ch1_int = []
 ch2_int = []
 ch3_int = []
-time_list = [step * time_step for step in range(len(images_list))] if ax_type == 't' else []
+time_list = [step * time_step + initial_time for step in range(len(images_list))] if ax_type == 't' else []
 
 for img in images_list:
     ch1_data, ch2_data, ch3_data = get_data(os.path.join(dir2work, img))
@@ -45,17 +57,19 @@ print(f'Ch1 intensity {ch1_int}')
 print(f'Ch2 intensity {ch2_int}')
 print(f'Ch3 intensity {ch3_int}')
 
+# x = [''for i in arange(78.145, 87.520, 0.625)] #40264_data
+
 plt.subplot(3, 1, 1)
 plt.title('I(t) channel 1 (728)' if ax_type == 't' else 'I(N) channel 1 (728)')
-plt.plot(ch1_int, 'r.-') if ax_type == 't' else plt.plot(time_list, ch1_int, 'r.-')
+plt.plot(time_list, ch1_int, 'r.-') if ax_type == 't' else plt.plot(ch1_int, 'r.-')
 
 plt.subplot(3, 1, 2)
 plt.title('I(t) channel 2 (706)' if ax_type == 't' else 'I(N) channel 2 (706)')
-plt.plot(ch2_int, 'r.-') if ax_type == 't' else plt.plot(time_list, ch2_int, 'r.-')
+plt.plot(time_list, ch2_int, 'r.-') if ax_type == 't' else plt.plot(ch2_int, 'r.-')
 
 plt.subplot(3, 1, 3)
 plt.title('I(t) channel 3 (667)' if ax_type == 't' else 'I(N) channel 3 (667)')
-plt.plot(ch3_int, 'r.-') if ax_type == 't' else plt.plot(time_list, ch3_int, 'r.-')
+plt.plot(time_list, ch3_int, 'r.-') if ax_type == 't' else plt.plot(ch3_int, 'r.-')
 
 plt.tight_layout()
 plt.show()
