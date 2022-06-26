8cd3cb47c35e94d9c28aef02868e380badd82340
joana <joana.fedorenko@gmail.com>
2022-06-26 18:47:21 +0300

    test change

--- app2rip/example.py
+++ app2rip/example.py
@@ -1,10 +1,10 @@
 import ripper
 
-data, link = ripper.extract('', 38516, ['^i', 'SXR'])
+# data, link = ripper.extract('', 38516, ['^i', 'SXR'])
+#
+# print(list(data.keys()), link)
 
-print(list(data.keys()), link)
-
-data_all = ripper.extract('in', 38516)  # extract all charts of 38516 discharge in relative path 'in'
+data_all = ripper.extract(r'C:\Users\joana\PycharmProjects\He_multich_analysis\app2rip', 40264)  # extract all charts of 38516 discharge in relative path 'in'
 
 for diag_list in data_all:
     for diag in diag_list:
