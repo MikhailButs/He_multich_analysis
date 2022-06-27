2b071e591b2b4f71753b2ac25ebb14880aa33ff1
misha <misha.bucz@mail.ru>
2022-06-27 11:57:02 +0300

    shows N2, He, D-alpha

--- app2rip/app2rip.py
+++ app2rip/app2rip.py
@@ -1,10 +1,27 @@
 import ripper as rp
 import matplotlib.pyplot as plt
+from os import path
 
-data, link = rp.extract(r'C:\Users\joana\PycharmProjects\He_multich_analysis\app2rip', 40264,
-                        ['D-alpha', 'D-alfa', 'He', 'N II'])
+sht_dir, sht_num = path.split(r'D:\home\projects\He_multich_analysis\app2rip\sht40264.SHT')
+sht_num = int(sht_num[3:-4])
+data, link = rp.extract(sht_dir, sht_num,
+                        ['D-alfa верхний купол', 'Газонапуск He капиляр', 'N II'])
 
 x, y = rp.x_y(data[59])
+plt.subplot(3, 1, 1)
+plt.plot(x, y)
+plt.title('D-alpha верхний купол')
+
+x, y = rp.x_y(data[35])
+plt.subplot(3, 1, 2)
+plt.plot(x, y)
+plt.title('N II')
+
+x, y = rp.x_y(data[81])
+plt.subplot(3, 1, 3)
+plt.plot(x, y)
+plt.title('Газонапуск He капилляр')
 
 plt.plot(y)
+plt.tight_layout()
 plt.show()
